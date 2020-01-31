a6cd4be8945acd0711ad76899cf7a49b3fbba399
Gentil Pinto <gentil.junior.gp@gmail.com>
2020-01-30 21:55:28 -0500

    index.php updated

--- public/index.php
+++ public/index.php
@@ -1,18 +1,4 @@
 <?php
 
-use app\classes\Layout;
-use app\classes\Route;
-use app\classes\Uri;
-
-require "../bootstrap.php";
-
-$routes = [
-    '/' => 'controllers/homeController',
-    '/control' => 'controllers/controlController'
-];
-
-$uri = Uri::load();
-$layout = new Layout;
-
-require Route::load($routes, $uri);
-require $layout->masterLayout('main');
+require __DIR__."/../vendor/autoload.php";
+require __DIR__."/../core/bootstrap.php";
